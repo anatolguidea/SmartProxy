b7e93f163a74732c7a0b257b4f13c3d8faf20dfc
Anatol Guidea <guidea.anatol@gmail.com>
2025-12-05 11:08:48 +0200

    added dockerfile

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -1,15 +1,36 @@
 using System;
+using System.Collections.Generic;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.ConfigureAppConfiguration(config => {
+    config.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+});
+
 var port = Environment.GetEnvironmentVariable("PORT") ?? "80";
 builder.WebHost.UseUrls($"http://*:{port}");
 
-builder.Host.ConfigureAppConfiguration(config => {
-    config.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+var movieApi1Host = Environment.GetEnvironmentVariable("MOVIEAPI1_HOST") ?? "movieapi1";
+var movieApi1Port = Environment.GetEnvironmentVariable("MOVIEAPI1_PORT") ?? "8080";
+var movieApi2Host = Environment.GetEnvironmentVariable("MOVIEAPI2_HOST") ?? "movieapi2";
+var movieApi2Port = Environment.GetEnvironmentVariable("MOVIEAPI2_PORT") ?? "8080";
+var downstreamScheme = Environment.GetEnvironmentVariable("DOWNSTREAM_SCHEME") ?? "http";
+
+builder.Configuration.AddInMemoryCollection(new Dictionary<string, string?>
+{
+    ["Routes:0:DownstreamHostAndPorts:0:Host"] = movieApi1Host,
+    ["Routes:0:DownstreamHostAndPorts:0:Port"] = movieApi1Port,
+    ["Routes:0:DownstreamHostAndPorts:1:Host"] = movieApi2Host,
+    ["Routes:0:DownstreamHostAndPorts:1:Port"] = movieApi2Port,
+    ["Routes:0:DownstreamScheme"] = downstreamScheme,
+    ["Routes:1:DownstreamHostAndPorts:0:Host"] = movieApi1Host,
+    ["Routes:1:DownstreamHostAndPorts:0:Port"] = movieApi1Port,
+    ["Routes:1:DownstreamHostAndPorts:1:Host"] = movieApi2Host,
+    ["Routes:1:DownstreamHostAndPorts:1:Port"] = movieApi2Port,
+    ["Routes:1:DownstreamScheme"] = downstreamScheme
 });
 
 builder.Services.AddControllers();
